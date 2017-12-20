1096c9e462e6143d4bf1eee7d69d08a48cf86ea7
Sean Cross <sean@xobs.io>
2017-12-20 10:23:09 +0800

    main: add option to log all bus messages
    This feature will be used to determine why things aren't working in
    certain situations.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate ctrlc;
 extern crate clap;
+extern crate ctrlc;
 
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -15,12 +15,12 @@ mod terminal;
 mod config;
 mod quiesce;
 
-use unitbroadcaster::{UnitEvent, UnitBroadcaster};
+use unitbroadcaster::{UnitBroadcaster, UnitEvent};
 use unitwatcher::UnitWatcher;
 use unitloader::UnitLoader;
 use unitlibrary::UnitLibrary;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn main() {
     let config = Arc::new(Mutex::new(config::Config::new()));
@@ -35,31 +35,44 @@ fn main() {
     // applied to all threads.
     let ctrl_c_broadcaster = unit_broadcaster.clone();
     ctrlc::set_handler(move || {
-            ctrl_c_broadcaster.broadcast(&UnitEvent::Shutdown);
-        })
-        .expect("Error setting Ctrl-C handler");
+        ctrl_c_broadcaster.broadcast(&UnitEvent::Shutdown);
+    }).expect("Error setting Ctrl-C handler");
 
     let matches = App::new("Exclave Testing System")
         .version("1.0")
         .author("Sean Cross <sean@xobs.io>")
         .about("Orchestrates the Common Factory Test Interface server")
-        .arg(Arg::with_name("CONFIG_DIR")
-            .short("c")
-            .long("config-dir")
-            .value_name("CONFIG_DIR")
-            .number_of_values(1)
-            .required(true)
-            .multiple(true)
-            .takes_value(true)
-            .help("Directory where configuration unit files are stored"))
-        .arg(Arg::with_name("PLAIN")
-            .short("p")
-            .long("plain-output")
-            .help("Force output to be 'plain' (rather than auto-detected)"))
-        .arg(Arg::with_name("QUIET")
-            .short("q")
-            .long("no-output")
-            .help("Prevent console output entirely"))
+        .arg(
+            Arg::with_name("CONFIG_DIR")
+                .short("c")
+                .long("config-dir")
+                .value_name("CONFIG_DIR")
+                .number_of_values(1)
+                .required(true)
+                .multiple(true)
+                .takes_value(true)
+                .help("Directory where configuration unit files are stored"),
+        )
+        .arg(
+            Arg::with_name("PLAIN")
+                .short("p")
+                .long("plain-output")
+                .help("Force output to be 'plain' (rather than auto-detected)"),
+        )
+        .arg(
+            Arg::with_name("QUIET")
+                .short("q")
+                .long("no-output")
+                .help("Prevent console output entirely"),
+        )
+        .arg(
+            Arg::with_name("DEBUG_LOGFILE")
+                .short("9")
+                .long("debug-log")
+                .help("Log all internal messages to the specified file")
+                .required(false)
+                .takes_value(true),
+        )
         .get_matches();
 
     let config_dirs: Vec<_> = matches.values_of("CONFIG_DIR").unwrap().collect();
@@ -74,24 +87,50 @@ fn main() {
     terminal::TerminalInterface::start(output_type, unit_broadcaster.subscribe());
 
     for config_dir in config_dirs {
-        unit_watcher.add_path(config_dir).expect("Unable to add config directory");
+        unit_watcher
+            .add_path(config_dir)
+            .expect("Unable to add config directory");
     }
 
     let mut quiesce = quiesce::Quiesce::new(Duration::from_secs(1), &unit_broadcaster);
 
     unit_broadcaster.log("main", format!("Exclave initializing"));
 
-    use std::fs::File;
-    use std::path::Path;
-    use std::io::Write;
-    let fname = "log.txt";
-    let path = Path::new(fname);
-    let mut file = File::create(&path).expect("Couldn't create logfile");
+    let mut debug_file = match matches.value_of("DEBUG_LOGFILE") {
+        None => None,
+        Some(dv) => {
+            use std::fs::File;
+            use std::path::Path;
+            let path = Path::new(dv);
+            Some(File::create(&path).expect("Couldn't create logfile"))
+        }
+    };
     // Main message loop.  Monitor messages and pass them to each component.
-    let mut i = 1;
+    let mut loops = 1;
     while let Ok(msg) = message_receiver.recv() {
-        writeln!(file, "Got message {}: {:?}", i, msg).expect("Couldn't write message to logfile");
-        i = i + 1;
+        if let Some(file) = debug_file.as_mut() {
+            use std::io::Write;
+            use std::time;
+
+            let now = time::SystemTime::now();
+            let now = match now.duration_since(time::UNIX_EPOCH) {
+                Ok(d) => d,
+                Err(_) => time::Duration::new(0, 0),
+            };
+
+            let unix_time = now.as_secs();
+            let unix_time_nsecs = now.subsec_nanos();
+
+            writeln!(
+                file,
+                "{}:{}.{} {:?}",
+                loops,
+                unix_time,
+                unix_time_nsecs,
+                msg
+            ).expect("Couldn't write message to logfile");
+        }
+        loops = loops + 1;
         unit_loader.process_message(&msg);
         unit_library.process_message(&msg);
         quiesce.process_message(&msg);
