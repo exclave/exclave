36f5a3af59a2c79d4c61cbefd7f2f89d8971d8f2
Sean Cross <sean@xobs.io>
2018-10-22 15:43:46 +0800

    units: interface: add unit_directory for path defaults
    Add a new parameter: unit_directory.  This is the default
    working_directory if no other paths are specified.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/units/interface.rs
+++ src/units/interface.rs
@@ -3,21 +3,24 @@ extern crate systemd_parser;
 
 use std::cell::RefCell;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Write, Error, ErrorKind};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read, Write};
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
 
 use config::Config;
-use unit::{UnitActivateError, UnitDeactivateError, UnitDescriptionError, UnitIncompatibleReason, UnitSelectError, UnitDeselectError,
-           UnitName};
-use unitmanager::{ManagerControlMessage, ManagerControlMessageContents, ManagerStatusMessage,
-                  UnitManager};
+use unit::{
+    UnitActivateError, UnitDeactivateError, UnitDescriptionError, UnitDeselectError,
+    UnitIncompatibleReason, UnitName, UnitSelectError,
+};
+use unitmanager::{
+    ManagerControlMessage, ManagerControlMessageContents, ManagerStatusMessage, UnitManager,
+};
 
-use self::systemd_parser::items::DirectiveEntry;
-use self::runny::Runny;
 use self::runny::running::{Running, RunningOutput};
+use self::runny::Runny;
+use self::systemd_parser::items::DirectiveEntry;
 
 #[derive(Clone, Copy)]
 enum InterfaceFormat {
@@ -48,6 +51,9 @@ pub struct InterfaceDescription {
 
     /// The working directory to start from when running the interface
     working_directory: Option<PathBuf>,
+
+    /// The path of the unit file
+    unit_directory: PathBuf,
 }
 
 impl InterfaceDescription {
@@ -71,6 +77,7 @@ impl InterfaceDescription {
             format: InterfaceFormat::Text,
             exec_start: "".to_owned(),
             working_directory: None,
+            unit_directory: path.parent().unwrap().to_owned(),
         };
 
         for entry in unit_file.lookup_by_category("Interface") {
@@ -90,8 +97,13 @@ impl InterfaceDescription {
                         }
                     }
                     "WorkingDirectory" => {
-                        interface_description.working_directory =
-                            Some(Path::new(directive.value().unwrap_or("")).to_owned())
+                        // If a WorkingDirectory was specified, add it to the current directory
+                        // (replaces `working_directory` if the new WD is absolute)
+                        if let Some(wd) = directive.value() {
+                            interface_description
+                                .working_directory
+                                = Some(PathBuf::from(wd));
+                        }
                     }
                     "ExecStart" => {
                         interface_description.exec_start = match directive.value() {
@@ -226,7 +238,11 @@ impl Interface {
         *self.process.borrow_mut() = Some(running);
 
         // Send some initial configuration to the client.
-        control_sender.send(ManagerControlMessage::new(&control_sender_id, ManagerControlMessageContents::InitialGreeting)).ok();
+        control_sender
+            .send(ManagerControlMessage::new(
+                &control_sender_id,
+                ManagerControlMessageContents::InitialGreeting,
+            )).ok();
 
         Ok(())
     }
@@ -276,17 +292,23 @@ impl Interface {
 
         match msg {
             ManagerStatusMessage::Jig(j) => match j {
-                Some(jig_name) => writeln!(process, "JIG {}", Self::cfti_escape(&format!("{}", jig_name))),
+                Some(jig_name) => writeln!(
+                    process,
+                    "JIG {}",
+                    Self::cfti_escape(&format!("{}", jig_name))
+                ),
                 None => writeln!(process, "JIG"),
             },
-            ManagerStatusMessage::Hello(id) => writeln!(process, "HELLO {}", Self::cfti_escape(&format!("{}", id))),
+            ManagerStatusMessage::Hello(id) => {
+                writeln!(process, "HELLO {}", Self::cfti_escape(&format!("{}", id)))
+            }
             ManagerStatusMessage::Tests(scenario, tests) => {
                 write!(process, "TESTS {}", Self::cfti_escape(scenario.id()))?;
                 for test in &tests {
                     write!(process, " {}", Self::cfti_escape(test.id()))?;
                 }
                 writeln!(process, "")
-            },
+            }
             ManagerStatusMessage::Scenario(name) => match name {
                 Some(s) => writeln!(process, "SCENARIO {}", Self::cfti_escape(s.id())),
                 None => writeln!(process, "SCENARIO"),
@@ -297,10 +319,12 @@ impl Interface {
                     write!(process, " {}", Self::cfti_escape(scenario_name.id()))?;
                 }
                 writeln!(process, "")
-            },
-            ManagerStatusMessage::Describe(id, field, value) => {
-                writeln!(process, "DESCRIBE {}", Self::cfti_escape(&format!("{} {} {} {}", id.kind(), field, id.id(), value)))
             }
+            ManagerStatusMessage::Describe(id, field, value) => writeln!(
+                process,
+                "DESCRIBE {}",
+                Self::cfti_escape(&format!("{} {} {} {}", id.kind(), field, id.id(), value))
+            ),
             ManagerStatusMessage::Log(l) => writeln!(
                 process,
                 "LOG {}\t{}\t{}\t{}\t{}\t{}",
@@ -311,22 +335,37 @@ impl Interface {
                 l.nsecs(),
                 Self::cfti_escape(l.message())
             ),
-            ManagerStatusMessage::Running(test) => writeln!(process, "RUNNING {}", Self::cfti_escape(test.id())),
-            ManagerStatusMessage::Skipped(test, reason) => {
-                writeln!(process, "SKIP {} {}", Self::cfti_escape(test.id()), Self::cfti_escape(&reason))
-            },
-            ManagerStatusMessage::Finished(scenario, result, reason) => {
-                writeln!(process, "FINISH {} {} {}", Self::cfti_escape(scenario.id()), result, Self::cfti_escape(&reason))
-            },
-            ManagerStatusMessage::Fail(test, _code, reason) => {
-                writeln!(process, "FAIL {} {}",Self::cfti_escape(test.id()), Self::cfti_escape(&reason))
+            ManagerStatusMessage::Running(test) => {
+                writeln!(process, "RUNNING {}", Self::cfti_escape(test.id()))
             }
-            ManagerStatusMessage::Pass(test, reason) => {
-                writeln!(process, "PASS {} {}", Self::cfti_escape(test.id()), Self::cfti_escape(&reason))
-            }
+            ManagerStatusMessage::Skipped(test, reason) => writeln!(
+                process,
+                "SKIP {} {}",
+                Self::cfti_escape(test.id()),
+                Self::cfti_escape(&reason)
+            ),
+            ManagerStatusMessage::Finished(scenario, result, reason) => writeln!(
+                process,
+                "FINISH {} {} {}",
+                Self::cfti_escape(scenario.id()),
+                result,
+                Self::cfti_escape(&reason)
+            ),
+            ManagerStatusMessage::Fail(test, _code, reason) => writeln!(
+                process,
+                "FAIL {} {}",
+                Self::cfti_escape(test.id()),
+                Self::cfti_escape(&reason)
+            ),
+            ManagerStatusMessage::Pass(test, reason) => writeln!(
+                process,
+                "PASS {} {}",
+                Self::cfti_escape(test.id()),
+                Self::cfti_escape(&reason)
+            ),
             ManagerStatusMessage::Start(scenario) => {
                 writeln!(process, "START {}", Self::cfti_escape(scenario.id()))
-            }             /*
+            } /*
             //            BroadcastMessageContents::Hello(name) => writeln!(stdin,
             //                                                "HELLO {}", name),
             //            BroadcastMessageContents::Ping(val) => writeln!(stdin,
@@ -344,21 +383,45 @@ impl Interface {
 
         for c in msg.chars() {
             was_bs = match c {
-                '\\' => if was_bs { out.push('\\'); false } else { true },
-                't' => { out.push(if was_bs { '\t' } else { 't' }); false },
-                'r' => { out.push(if was_bs { '\r' } else { 'r' }); false },
-                'n' => { out.push(if was_bs { '\n' } else { 'n' }); false },
-                _ => {out.push(c); false },
+                '\\' => if was_bs {
+                    out.push('\\');
+                    false
+                } else {
+                    true
+                },
+                't' => {
+                    out.push(if was_bs { '\t' } else { 't' });
+                    false
+                }
+                'r' => {
+                    out.push(if was_bs { '\r' } else { 'r' });
+                    false
+                }
+                'n' => {
+                    out.push(if was_bs { '\n' } else { 'n' });
+                    false
+                }
+                _ => {
+                    out.push(c);
+                    false
+                }
             };
         }
         out
     }
 
-    fn text_read_stderr(id: UnitName, control: Sender<ManagerControlMessage>, output: RunningOutput) {
+    fn text_read_stderr(
+        id: UnitName,
+        control: Sender<ManagerControlMessage>,
+        output: RunningOutput,
+    ) {
         for line in BufReader::new(output).lines() {
             let line = line.expect("Unable to get next line");
             // If the send fails, that means the other end has closed the pipe.
-            if let Err(_) = control.send(ManagerControlMessage::new(&id, ManagerControlMessageContents::LogError(line))) {
+            if let Err(_) = control.send(ManagerControlMessage::new(
+                &id,
+                ManagerControlMessageContents::LogError(line),
+            )) {
                 break;
             }
         }
@@ -367,7 +430,8 @@ impl Interface {
     fn text_read(id: UnitName, control: Sender<ManagerControlMessage>, stdout: RunningOutput) {
         for line in BufReader::new(stdout).lines() {
             let line = line.expect("Unable to get next line");
-            let mut words: Vec<String> = line.split_whitespace()
+            let mut words: Vec<String> = line
+                .split_whitespace()
                 .map(|x| Self::cfti_unescape(x.to_owned()))
                 .collect();
 
@@ -381,29 +445,53 @@ impl Interface {
 
             let response = match verb.as_str() {
                 "scenarios" => ManagerControlMessageContents::Scenarios,
-                "scenario" => match UnitName::from_str(words.get(0).unwrap_or(&"".to_owned()).to_lowercase().as_str(), "scenario") {
-                        Err(e) => ManagerControlMessageContents::Error(format!("Invalid scenario name: {}", e)),
-                        Ok(o) => ManagerControlMessageContents::Scenario(o),
-                    }
-                ,
+                "scenario" => match UnitName::from_str(
+                    words
+                        .get(0)
+                        .unwrap_or(&"".to_owned())
+                        .to_lowercase()
+                        .as_str(),
+                    "scenario",
+                ) {
+                    Err(e) => ManagerControlMessageContents::Error(format!(
+                        "Invalid scenario name: {}",
+                        e
+                    )),
+                    Ok(o) => ManagerControlMessageContents::Scenario(o),
+                },
                 "tests" => {
                     if words.is_empty() {
                         ManagerControlMessageContents::Tests(None)
                     } else {
                         match UnitName::from_str(words[0].to_lowercase().as_str(), "test") {
-                            Ok(scenario_name) => ManagerControlMessageContents::Tests(Some(scenario_name)),
-                            Err(e) => ManagerControlMessageContents::Error(format!("Invalid test name specified: {}", e)),
+                            Ok(scenario_name) => {
+                                ManagerControlMessageContents::Tests(Some(scenario_name))
+                            }
+                            Err(e) => ManagerControlMessageContents::Error(format!(
+                                "Invalid test name specified: {}",
+                                e
+                            )),
                         }
                     }
-                },
+                }
                 "jig" => ManagerControlMessageContents::Jig,
                 "log" => ManagerControlMessageContents::Log(words.join(" ")),
                 "start" => {
                     if words.is_empty() {
                         ManagerControlMessageContents::StartScenario(None)
                     } else {
-                        match UnitName::from_str(words.get(0).unwrap_or(&"".to_owned()).to_lowercase().as_str(), "scenario") {
-                            Err(e) => ManagerControlMessageContents::Error(format!("Invalid scenario name: {}", e)),
+                        match UnitName::from_str(
+                            words
+                                .get(0)
+                                .unwrap_or(&"".to_owned())
+                                .to_lowercase()
+                                .as_str(),
+                            "scenario",
+                        ) {
+                            Err(e) => ManagerControlMessageContents::Error(format!(
+                                "Invalid scenario name: {}",
+                                e
+                            )),
                             Ok(o) => ManagerControlMessageContents::StartScenario(Some(o)),
                         }
                     }
@@ -428,6 +516,10 @@ impl Interface {
                 break;
             }
         }
-        control.send(ManagerControlMessage::new(&id, ManagerControlMessageContents::ChildExited)).expect("interface couldn't send exit message to controller");
+        control
+            .send(ManagerControlMessage::new(
+                &id,
+                ManagerControlMessageContents::ChildExited,
+            )).expect("interface couldn't send exit message to controller");
     }
 }
