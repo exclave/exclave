031cc0c1c7bbdae1214a1a956b2aa4a4cd033c50
Sean Cross <sean@xobs.io>
2018-10-30 09:50:07 +0800

    ci: disable extra targets, add more build.rs output
    Try to debug Travis by adding more debug output and disabling extra
    targets.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- build.rs
+++ build.rs
@@ -10,19 +10,27 @@ use std::process::Command;
 /// fn main() { git_version::set_env_with_name("CARGO_PKG_VERSION"); }
 /// ```
 fn set_env_with_name(name: &str) {
-    let cmd = Command::new("git")
+    let cmd = match Command::new("git")
         .args(&["describe", "--tags", "--dirty=-modified"])
         .output()
-        .unwrap();
+    {
+        Ok(rev) => rev,
+        Err(e) => panic!("unable to run git to get package version: {:?}", e),
+    };
     assert!(cmd.status.success());
-    let ver = std::str::from_utf8(&cmd.stdout[..]).unwrap().trim();
+    let ver = match std::str::from_utf8(&cmd.stdout[..]) {
+        Ok(v) => v.trim(),
+        Err(e) => panic!("unable to convert version number from utf8: {:?}", e),
+    };
     println!("cargo:rustc-env={}={}", name, ver);
-    println!("cargo:rerun-if-changed=.git/HEAD"); 
-    println!("cargo:rerun-if-changed=.git/index"); 
-    println!("cargo:rerun-if-changed=.git"); 
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/index");
+    println!("cargo:rerun-if-changed=.git");
     println!("cargo:rerun-if-env-changed={}", name);
 }
 
+use std::env;
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
     set_env_with_name("GIT_VERSION");
 }
